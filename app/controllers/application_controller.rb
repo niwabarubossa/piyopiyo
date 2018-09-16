9c04c4b56db4de4a6f46ccc0cc7132b34bc8a5a6
Ryogo Niwa <niwabarubossa@yahoo.co.jp>
2018-09-16 00:50:10 +0000

    branchtest commit application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
 
   def index
     puts "I am sample2!!!!!!!!!"
+    puts " this is a git branch test 　今N2iで作業している状態を再現　これが今ブランチで作業して変更したところ"
   end
 
 end
